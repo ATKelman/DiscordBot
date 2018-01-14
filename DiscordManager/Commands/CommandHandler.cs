4e96aec3728c436a8f7829b432a9e42200eebda2
ATKelman <a14aleke@student.his.se>
2018-01-14 17:18:37 +0100

    Setup of DiscordManager Project

---
+++ DiscordManager/Commands/CommandHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using System.Reflection;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordManager
+{
+    class CommandHandler
+    {
+        private static CommandService _cmds;
+        private DiscordSocketClient _client;
+
+        public async Task Install(DiscordSocketClient c)
+        {
+            _client = c;
+            _cmds = new CommandService();
+
+            await _cmds.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommand;
+        }
+
+        public async Task HandleCommand(SocketMessage e)
+        {
+            SocketUserMessage msg = e as SocketUserMessage;
+            if (msg == null) return;
+
+            CommandContext context = new CommandContext(_client, msg);
+
+            int argPos = 0;
+            if (msg.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _cmds.ExecuteAsync(context, argPos);
+
+                if (!result.IsSuccess) await context.Channel.SendMessageAsync(result.ErrorReason.ToString());
+            }
+        }
+
+        public static CommandService GetCommandService()
+        {
+            return _cmds;
+        }
+
+        //private static IEnumerable<char> GetCharsInRange(string text, int min, int max)
+        //{
+        //    return text.Where(x => x >= min && x <= max);
+        //}
+
+        //var hiragana = GetCharsInRange(context.Message.ToString(), 0x3040, 0x309F);
+        //if (hiragana.Count() > 0)
+        //{
+        //    Console.WriteLine("found hiragana");
+        //    Console.WriteLine(msg.Content);
+        //    //Console.WriteLine(msg2.Content);
+        //    Console.WriteLine(context.ToString());
+
+        //    var result = await _cmds.ExecuteAsync(context, 0);
+
+        //    if (!result.IsSuccess) await context.Channel.SendMessageAsync(result.ErrorReason.ToString());
+        //}
+
+
+
+        /*
+        var romaji = GetCharsInRange(searchKeyword, 0x0020, 0x007E);
+        var hiragana = GetCharsInRange(searchKeyword, 0x3040, 0x309F);
+        var katakana = GetCharsInRange(searchKeyword, 0x30A0, 0x30FF);
+        var kanji = GetCharsInRange(searchKeyword, 0x4E00, 0x9FBF);
+         */
+    }
+}
---
+++ DiscordManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start().GetAwaiter().GetResult();
+        }
+
+        private DiscordSocketClient _client;
+        private CommandHandler _commands;
+
+        public async Task Start()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, ConfigurationManager.AppSettings["Token"]);
+            //await _client.LoginAsync(TokenType.Bot, "MzE5ODk1ODM2ODM2MzY0Mjk5.DBHluw.CckE04fI_3xLwlu0SPzXDKlPHAQ");
+            await _client.StartAsync();
+
+            _client.Log += Log;
+
+            await _commands.Install(_client);
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return null;
+        }
+    }
+}
