1db321358de44ec4ad2225cd6a937ebd4e8b1bd7
ATKelman <a14aleke@student.his.se>
2018-04-05 20:53:10 +0200

    Created Reddit Command - no code as of yet

---
+++ DiscordManager/Commands/RedditCommand.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace DiscordManager.Commands
+{
+    public class RedditCommand : ModuleBase
+    {
+    }
+}
